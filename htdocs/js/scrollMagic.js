73dee478f0890b5066aa827b0b7a6465821c2f68
Hisashi <hisasi622@gmail.com>
2021-06-05 23:44:00 +0900

    特定のクラスがついているときに発火するように

--- htdocs/js/scrollMagic.js
+++ htdocs/js/scrollMagic.js
@@ -1,10 +1,29 @@
-var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({
-  triggerElement: '#act',
-  offset: 100,
-  triggerHook: 'onCenter', // トリガーフック指定
-  reverse: false,
-})
-  .setClassToggle('#act', 'fixed')
-  .addIndicators()
-  .addTo(controller); // Controllerに追加
+class ScrollFade {
+  constructor() {
+    this.ANIMATION_CLASS = 'active';
+
+    let $section = document.querySelectorAll('.box:not(.active)');
+    if ($section.length === null) {
+      return;
+    }
+    let controller = new ScrollMagic.Controller();
+    for (let i = 0; i < $section.length; i++) {
+      let scene = new ScrollMagic.Scene({
+        triggerElement: $section[i],
+        triggerHook: 'onEnter',
+        reverse: false,
+        offset: 150,
+      })
+        .addIndicators()
+        .addTo(controller);
+      scene.on('enter', () => {
+        $section[i].classList.toggle(this.ANIMATION_CLASS);
+      });
+      scene.on('end', () => {
+        scene.destroy(false);
+      });
+    }
+  }
+}
+
+new ScrollFade();
